7d10de8915ab24eb0f0bca5c841817cdbe3b92da
Icaro Kossman <icaro.kossman@gcs.local>
2026-04-23 15:43:12 -0300

    Primeiro Commit

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+
+Console.WriteLine("Use input:" + userChoice);
+userChoice = "ABCDEFGH";
+
+Console.ReadKey();
